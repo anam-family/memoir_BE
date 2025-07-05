77c8244958c5841b80acabb41e1ba80dd4f0cc1d
hyun.lee <qwe7105c@naver.com>
2025-07-06 05:15:27 +0900

    테스트 환경 설정 추가: H2 메모리 DB 사용으로 테스트 문제 해결

--- src/test/java/com/memoir/memoir/MemoirApplicationTests.java
+++ src/test/java/com/memoir/memoir/MemoirApplicationTests.java
@@ -2,12 +2,15 @@ package com.memoir.memoir;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class MemoirApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// 애플리케이션 컨텍스트가 정상적으로 로드되는지 확인
 	}
 
 }
